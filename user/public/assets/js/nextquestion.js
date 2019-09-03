6d7b9046a1d80aae0802d90ef2209dea5f6ed142
vipin95 <mavi.mymail@gmail.com>
2019-09-03 08:41:55 +0530

    change questions formate

--- user/public/assets/js/nextquestion.js
+++ user/public/assets/js/nextquestion.js
@@ -14,44 +14,27 @@ function nextQuestion(){
     let datastr = document.getElementById('hiddenData').innerHTML;
     let data = JSON.parse(datastr)
     let nextQuestion = qNumber+1+" of 20";
-     
+    
+    document.getElementById('radio1').checked=true;
+    console.log(document.getElementById("img_ques").src);
     let currect_ans = checkAnswer(question,answers,qNumber,data);
 
     for(let i=0 ; i<data.length ; i++){
         if(i==qNumber){
             // document.getElementById("question").innerHTML = data[i].question;
             if( data[i].ques_image ){
-                document.querySelector('#test_question').innerHTML = `<div class="row">
-                <div class="col-md-10">
-                    <div class="mr-3" style="font-size: 152%;">Question : <strong id="questionNumber"> ${nextQuestion} </strong></div>
-                    <div class="media-body">
-                        <h5 class="mt-0"></h5>
-                        <p id="question"> ${data[i].question} </p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <img src="public/assets/question_ans_img/${data[i].ques_image_name}" alt="image not loeded">
-                </div>
-            </div>`;
+                document.getElementById("question").innerHTML = data[i].question;
+                document.getElementById("img_ques").src = `public/assets/question_ans_img/${data[i].ques_image_name}`;
             }
             else{
-                document.querySelector('#test_question').innerHTML =`<div class="row">
-                <div class="col-md-12">
-                    <div class="mr-3" style="font-size: 152%;">Question : <strong id="questionNumber"> ${nextQuestion} </strong></div>
-                    <div class="media-body">
-                        <h5 class="mt-0"></h5>
-                        <div class="card-block">
-                            <p id="question"> ${data[i].question} </p>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
+                document.getElementById("question").innerHTML = data[i].question;
+                document.getElementById("img_ques").src = ``;
             }
             options_value[0].innerHTML = data[i].option1;
             options_value[1].innerHTML = data[i].option2;
             options_value[2].innerHTML = data[i].option3;
             options_value[3].innerHTML = data[i].option4;
-            // document.getElementById('questionNumber').innerHTML = nextQuestion;
+            document.getElementById('questionNumber').innerHTML = nextQuestion;
         }
     }
     if(qNumber == data.length){
